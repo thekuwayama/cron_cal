376902c4dfedd0a98fdfd68a26e4ab0ab0bca63a
thekuwayama <thekuwayama@gmail.com>
2022-03-28 07:31:47 +0900

    refactor: using unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -28,22 +28,25 @@ fn main() {
             NaiveDate::parse_from_str(s, "%Y-%m-%d")
                 .map(|n| DateTime::<Utc>::from_utc(n.and_hms(0, 0, 0), Utc))
         })
-        .unwrap_or(Ok(Utc::today().and_hms(0, 0, 0)))
-        .expect("Failed to parse date option");
-    // input
+        .unwrap_or_else(|| Ok(Utc::today().and_hms(0, 0, 0)))
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to parse date option: {}", e);
+            process::exit(1);
+        });
+    // Read input
     let stdin = io::stdin();
     let mut buf = String::new();
-    stdin
-        .lock()
-        .read_to_string(&mut buf)
-        .expect("Failed to read stdin");
+    stdin.lock().read_to_string(&mut buf).unwrap_or_else(|e| {
+        eprintln!("Failed to read stdin: {}", e);
+        process::exit(1);
+    });
     let mut input = buf.as_bytes();
-    // parse
+    // Parse input
     let cal = parse::parse(&mut input, date).unwrap_or_else(|e| {
         eprintln!("{}", e);
         process::exit(1);
     });
-    // output
+    // Print result
     println!(
         "{}",
         if spare {
