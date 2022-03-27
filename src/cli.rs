43f8abf8fd6f15f54ddf1589b047cd71bc0c3a87
thekuwayama <thekuwayama@gmail.com>
2022-03-27 22:07:17 +0900

    refactor: for cargo clippy

--- src/cli.rs
+++ src/cli.rs
@@ -68,7 +68,7 @@ pub(crate) fn format_cal(cal: &CronCalender, scale: usize) -> String {
             if b {
                 let start = today + Duration::minutes((i * scale) as i64);
                 let end = start + Duration::minutes(scale as i64);
-                Some(format!("{} ~ {}", start.to_string(), end.to_string()))
+                Some(format!("{} ~ {}", start, end))
             } else {
                 None
             }
