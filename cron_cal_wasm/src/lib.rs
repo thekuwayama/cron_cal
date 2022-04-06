1f2d35f7d40763db912210b269e3d675e13cc962
thekuwayama <thekuwayama@gmail.com>
2022-04-07 00:19:14 +0900

    feat: wasm args(date & days) (#7)

--- cron_cal_wasm/src/lib.rs
+++ cron_cal_wasm/src/lib.rs
@@ -1,19 +1,17 @@
 #![allow(clippy::unused_unit)]
 
-use chrono::offset::Utc;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use cron_cal::format::format_unix_timestamp;
 use cron_cal::parse::parse;
 use js_sys::{Array, BigInt, BigInt64Array};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn parse_cron_cal(input: &str) -> Result<BigInt64Array, JsValue> {
-    // TODO: Add args; days, date
-    let date = Utc::today().and_hms(0, 0, 0);
-    let days = 1;
+pub fn parse_cron_cal(input: &str, date: i64, days: i32) -> Result<BigInt64Array, JsValue> {
+    let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(date, 0), Utc);
     let mut input = input.as_bytes();
 
-    match parse(&mut input, date, days) {
+    match parse(&mut input, date, days as usize) {
         Ok(cal) => Ok(BigInt64Array::new(&JsValue::from(
             format_unix_timestamp(&cal, date)
                 .iter()
