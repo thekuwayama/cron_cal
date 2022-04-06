8c5a3386e2488bbb93739e41a5d05b58c95e5fa1
thekuwayama <thekuwayama@gmail.com>
2022-04-06 23:24:53 +0900

    fix: simplenize cli (#6)

--- cron_cal/src/main.rs
+++ cron_cal/src/main.rs
@@ -1,9 +1,7 @@
 use std::io::{self, prelude::*};
 use std::process;
-use std::str::FromStr;
 
-use chrono::naive::NaiveDate;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDate, NaiveDateTime, Utc};
 
 mod cli;
 mod format;
@@ -13,16 +11,6 @@ mod r#type;
 fn main() {
     // CLI init
     let matches = cli::build().get_matches();
-    let scale = matches
-        .value_of(cli::SCALE)
-        .expect("Failed to parse scale option");
-    let scale: usize = match cli::Scale::from_str(scale).unwrap_or(cli::Scale::Hour) {
-        cli::Scale::No => 0,
-        cli::Scale::Quarter => 15,
-        cli::Scale::Half => 30,
-        cli::Scale::Hour => 60,
-    };
-    let spare = matches.is_present(cli::SPARE);
     let date = matches
         .value_of(cli::DATE)
         .map(|s| {
@@ -56,15 +44,13 @@ fn main() {
         process::exit(1);
     });
     // Print result
-    if scale > 0 && spare {
-        format::format_rfc3339_rounding_spare(&cal, scale, date)
-    } else if scale > 0 {
-        format::format_rfc3339_rounding(&cal, scale, date)
-    } else if spare {
-        format::format_rfc3339_spare(&cal, date)
-    } else {
-        format::format_rfc3339(&cal, date)
-    }
-    .iter()
-    .for_each(|p| println!("{} ~ {}", p.0, p.1));
+    format::format_unix_timestamp(&cal, date)
+        .iter()
+        .for_each(|p| {
+            println!(
+                "{} ~ {}",
+                DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(p.0, 0), Utc),
+                DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(p.1, 0), Utc),
+            )
+        });
 }
