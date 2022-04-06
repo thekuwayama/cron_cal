7a1ce5464135f87c30e375a07acd7857db66db61
thekuwayama <thekuwayama@gmail.com>
2022-04-06 23:01:35 +0900

    fix: simplenize cli

--- cron_cal/src/cli.rs
+++ cron_cal/src/cli.rs
@@ -1,71 +1,12 @@
-use std::fmt::Display;
-use std::str::FromStr;
+use clap::{crate_description, crate_name, crate_version, Arg, Command};
 
-use clap::{crate_description, crate_name, crate_version, Arg, ArgEnum, Command, PossibleValue};
-
-pub(crate) const SCALE: &str = "scale";
-pub(crate) const SPARE: &str = "spare";
 pub(crate) const DATE: &str = "date";
 pub(crate) const DAYS: &str = "days";
-
-#[derive(ArgEnum, Clone, Copy)]
-pub(crate) enum Scale {
-    No,
-    Quarter,
-    Half,
-    Hour,
-}
-
-impl FromStr for Scale {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        for variant in Self::value_variants() {
-            if variant.to_possible_value().unwrap().matches(s, false) {
-                return Ok(*variant);
-            }
-        }
-        Err(format!("Invalid variant: {}", s))
-    }
-}
-
-impl Display for Scale {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.to_possible_value()
-            .expect("no values are skipped")
-            .get_name()
-            .fmt(f)
-    }
-}
-
-impl Scale {
-    pub fn possible_values() -> impl Iterator<Item = PossibleValue<'static>> {
-        Self::value_variants()
-            .iter()
-            .filter_map(ArgEnum::to_possible_value)
-    }
-}
 
 pub(crate) fn build() -> Command<'static> {
     Command::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
-        .arg(
-            Arg::new(SCALE)
-                .long(SCALE)
-                .short('s')
-                .help("scale of schedule")
-                .default_value("no")
-                .takes_value(true)
-                .possible_values(Scale::possible_values())
-                .required(false),
-        )
-        .arg(
-            Arg::new(SPARE)
-                .long(SPARE)
-                .help("print the spare time")
-                .takes_value(false)
-                .required(false),
-        )
         .arg(
             Arg::new(DATE)
                 .long(DATE)
--- cron_cal/src/main.rs
+++ cron_cal/src/main.rs
@@ -1,6 +1,5 @@
 use std::io::{self, prelude::*};
 use std::process;
-use std::str::FromStr;
 
 use chrono::naive::NaiveDate;
 use chrono::{DateTime, Utc};
@@ -13,16 +12,6 @@ mod r#type;
 fn main() {
     // CLI init
     let matches = cli::build().get_matches();
-    let scale = matches
-        .value_of(cli::SCALE)
-        .expect("Failed to parse scale option");
-    let scale: usize = match cli::Scale::from_str(scale).unwrap_or(cli::Scale::Hour) {
-        cli::Scale::No => 0,
-        cli::Scale::Quarter => 15,
-        cli::Scale::Half => 30,
-        cli::Scale::Hour => 60,
-    };
-    let spare = matches.is_present(cli::SPARE);
     let date = matches
         .value_of(cli::DATE)
         .map(|s| {
@@ -56,15 +45,7 @@ fn main() {
         process::exit(1);
     });
     // Print result
-    if scale > 0 && spare {
-        format::format_rfc3339_rounding_spare(&cal, scale, date)
-    } else if scale > 0 {
-        format::format_rfc3339_rounding(&cal, scale, date)
-    } else if spare {
-        format::format_rfc3339_spare(&cal, date)
-    } else {
-        format::format_rfc3339(&cal, date)
-    }
-    .iter()
-    .for_each(|p| println!("{} ~ {}", p.0, p.1));
+    format::format_unix_timestamp(&cal, date)
+        .iter()
+        .for_each(|p| println!("{} ~ {}", p.0, p.1));
 }
