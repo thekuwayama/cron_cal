b724a953a72ee7641011ff913da506ce62b44566
thekuwayama <thekuwayama@gmail.com>
2022-04-06 23:13:08 +0900

    improvement: error handling for cron format (#5)

--- cron_cal_wasm/src/lib.rs
+++ cron_cal_wasm/src/lib.rs
@@ -7,18 +7,20 @@ use js_sys::{Array, BigInt, BigInt64Array};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn parse_cron_cal(input: &str) -> BigInt64Array {
+pub fn parse_cron_cal(input: &str) -> Result<BigInt64Array, JsValue> {
     // TODO: Add args; days, date
     let date = Utc::today().and_hms(0, 0, 0);
     let days = 1;
     let mut input = input.as_bytes();
 
-    let cal = parse(&mut input, date, days).unwrap(); // FIXME: error handling
-    BigInt64Array::new(&JsValue::from(
-        format_unix_timestamp(&cal, date)
-            .iter()
-            .flat_map(|p| vec![p.0, p.1])
-            .map(BigInt::from)
-            .collect::<Array>(),
-    ))
+    match parse(&mut input, date, days) {
+        Ok(cal) => Ok(BigInt64Array::new(&JsValue::from(
+            format_unix_timestamp(&cal, date)
+                .iter()
+                .flat_map(|p| vec![p.0, p.1])
+                .map(BigInt::from)
+                .collect::<Array>(),
+        ))),
+        _ => Err(JsValue::from("cron format error")),
+    }
 }
