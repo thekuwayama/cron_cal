8616cb7bdc55eab0f119f22e9fbe176847beb7c0
thekuwayama <thekuwayama@gmail.com>
2022-04-09 22:04:15 +0900

    first implements

---
+++ cron_cal_wasm/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    entry: './www/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js?[hash]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./www/index.html"
+        })
+    ]
+};
